89268ecfa4193bb701c628d1776cfcb237402160
Yogeshkumar <ypatel@salesforce.com>
2017-02-20 21:40:32 -0800

    Added comments to files and updated readme

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # codeallday
-
-1) Is Unique: Implement algorithm to determine if a string has all unique characters. What if you cannot use additional
-structures?
-
-2) Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
--- src/main/java/com/codeallday/arrays/strings/CheckPermutation.java
+++ src/main/java/com/codeallday/arrays/strings/CheckPermutation.java
@@ -2,6 +2,9 @@ package com.codeallday.arrays.strings;
 
 /**
  * Created by ypatel on 2/20/17.
+ * Is Unique: Implement algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional structures?
  */
 public class CheckPermutation {
+
 }
